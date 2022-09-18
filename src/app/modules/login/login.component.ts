eb9bdaba1f78f3bca2a2ec9f8d737671fe23a27e
Ivan <ivan.gotchev@abv.bg>
2022-09-18 12:45:07 +0300

    parsed jwt token to local storage

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { JwtService } from 'src/app/services/jwt/jwt.service';
 import { UsersService } from 'src/app/services/users/users.service';
 
 @Component({
@@ -10,7 +11,10 @@ import { UsersService } from 'src/app/services/users/users.service';
 export class LoginComponent implements OnInit {
   public loginForm!: FormGroup;
 
-  constructor(private usersService: UsersService) { }
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+    ) { }
 
   ngOnInit(): void {
     this.buildLoginForm();
@@ -28,7 +32,12 @@ export class LoginComponent implements OnInit {
     const password = this.loginForm.controls['password'].value;
     this.usersService
       .loginUser(unsername, password)
-      .subscribe(console.log);
+      .subscribe((response) => {
+        const expiresIn = Object(response)['expiresIn'];
+        const accessId = Object(response)['access'];
+        this.jwtService.storeJwtToken(accessId, expiresIn);
+        console.log(this.jwtService.isLoggedIn());
+      });
   }
 }
 
---
+++ src/app/services/jwt/jwt.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JwtService {
+  public storeJwtToken(idToken: string, expiresIn: string) {
+    localStorage.setItem('id_token', idToken);
+    localStorage.setItem("expires_at", expiresIn);
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  private getExpiration() {
+    const expiration = localStorage.getItem("expires_at") as string;
+    return moment(expiration);
+  }  
+}
