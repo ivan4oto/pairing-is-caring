75a766ebbf9580449523018a4833ec51cab266b0
Ivan <redial@abv.bg>
2022-09-21 00:01:34 +0300

    row oriented and mock data

--- src/app/modules/paring-table/paring-table.component.ts
+++ src/app/modules/paring-table/paring-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from "lodash";
 import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag} from '@angular/cdk/drag-drop';
 
 @Component({
@@ -7,8 +8,9 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag} from '@angular
   styleUrls: ['./paring-table.component.scss']
 })
 export class ParingTableComponent implements OnInit {
-  public todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
+  // public todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  public pairs!: any[];
+  todoList3="todoList3"
   public people = [
     {name: 'ivan', pairs: ['gosho', 'mitko']},
     {name: 'mitko', pairs: ['gosho', 'ivan']},
@@ -17,21 +19,24 @@ export class ParingTableComponent implements OnInit {
     {name: 'mimi', pairs: ['petya']},
     {name: 'petya', pairs: ['mimi']}
   ]
+  
 
-  public constructLists() {
-    const resultList = new Set();
+  public constructLists(): any[] {
+    const resultList = new Array();
     this.people.forEach(obzekt => {
       console.log(obzekt)
-      resultList.add([...obzekt.pairs, obzekt.name].sort())
+      resultList.push([...obzekt.pairs, obzekt.name].sort())
     });
-    console.log(resultList)
-    return resultList;
+    const uniqarray = _.uniqWith(resultList, _.isEqual);
+    this.pairs = uniqarray;
+    return uniqarray;
   }
 
 
   public done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.constructLists();
@@ -47,6 +52,7 @@ export class ParingTableComponent implements OnInit {
         event.previousIndex,
         event.currentIndex,
       );
+      console.log(this.pairs);
     }
   }
 }
