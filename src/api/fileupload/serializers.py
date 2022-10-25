f7909178bf255ebf2c0f361f889fc3fbcf438d8a
Ivan <redial@abv.bg>
2022-10-25 21:40:13 +0300

    fix that file serializer

--- src/api/fileupload/serializers.py
+++ src/api/fileupload/serializers.py
@@ -1,14 +1,8 @@
 from rest_framework import serializers
-from fileupload.models import File
 
 
-class FileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = File
-        fields = ['id', 'file', 'file_name', 'file_type']
-        extra_kwargs = {
-            "id": {
-                "read_only": False,
-                "required": False,
-            }
-        }
+class FileSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    file = serializers.CharField()
+    file_name = serializers.CharField()
+    file_type = serializers.CharField()
