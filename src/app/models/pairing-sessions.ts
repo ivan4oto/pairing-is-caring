4d30e64f685c67216450e5142cc5f07cdcdf874e
ivan4oto <redial@abv.bg>
2023-03-16 15:37:44 +0200

    updating session model

--- src/app/models/pairing-sessions.ts
+++ src/app/models/pairing-sessions.ts
@@ -1,6 +1,8 @@
 export interface PairingSession {
     id: string;
-    start_time: string
+    start_time: string;
+    description: string;
+    title: string;
 }
 
 export interface PairingGroup {
